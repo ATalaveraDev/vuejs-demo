392332228e7c1283fbee5e9d0fb3e3f01cc0f5ed
Angel Talavera <atalavera@mimecast.com>
2017-05-21 13:09:58 +0100

    I - functionality for delete

--- src/App.vue
+++ src/App.vue
@@ -1,66 +1,73 @@
 <template>
-  <div id="app">
-    <div class="row">
-      <div class="col-lg-6">
-        <h2>List</h2>
-        <demo-list :books="books"></demo-list>
-      </div>
-      <div class="col-lg-6">
-        <h2>Form</h2>
-        <demo-form @movieAdded="updateList"></demo-form>
-      </div>
+    <div id="app">
+        <div class="row">
+            <div class="col-lg-6">
+                <h2>List</h2>
+                <demo-list :movies="movies" @movieDeleted="updateList('delete', $event)"></demo-list>
+            </div>
+            <div class="col-lg-6">
+                <h2>Form</h2>
+                <demo-form @movieAdded="updateList('add', $event)"></demo-form>
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-export default {
-  name: 'app',
-  data() {
-      return {
-          books: {
-              type: Array
-          }
-      }
-  },
-  beforeMount() {
-      this.$http.get('http://localhost:8081/movies').then(response => {
-          this.books = response.data;
-      });
-  },
-  methods: {
-      updateList(event) {
-          this.books.push(event);
-      }
-  }
-}
+    const constants = require('./helpers').default.constants;
+
+    export default {
+        data() {
+            return {
+                movies: {
+                    type: Array
+                }
+            }
+        },
+        beforeMount() {
+            this.$http.get(constants.endpoint).then(response => {
+                this.movies = response.data;
+            });
+        },
+        methods: {
+            updateList(action, movie) {
+                if (action === 'add') {
+                    this.movies.push(movie);
+                } else {
+                    this.movies = this.movies.filter(element => {
+                        return element._id !== movie._id;
+                    });
+                }
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 
-h1, h2 {
-  font-weight: normal;
-}
+    h1, h2 {
+        font-weight: normal;
+    }
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
 
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
+    li {
+        display: inline-block;
+        margin: 0 10px;
+    }
 
-a {
-  color: #42b983;
-}
+    a {
+        color: #42b983;
+    }
 </style>
--- src/components/List.vue
+++ src/components/List.vue
@@ -1,13 +1,31 @@
 <template>
     <div>
         <ul class="list-group">
-            <li v-for="book in books" class="list-group-item">{{ book.title }}</li>
+            <li v-for="movie in movies" class="list-group-item">
+                <span>{{ movie.title }}</span>
+                <span class="pull-right delete-btn" @click="deleteMovie(movie._id)">X</span>
+            </li>
         </ul>
     </div>
 </template>
 
 <script>
+    const constants = require('../helpers').default.constants;
+
     export default {
-        props: [ 'books' ]
+        props: [ 'movies' ],
+        methods: {
+            deleteMovie(id) {
+                this.$http.delete(constants.endpoint + '/' + id).then(response => {
+                    this.$emit('movieDeleted', response.data);
+                });
+            }
+        }
     }
 </script>
+
+<style>
+    .delete-btn {
+        cursor: pointer;
+    }
+</style>
