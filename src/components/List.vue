76b6ce729b55256475db0830263f02d2124c73fc
Angel Talavera <atalavera@mimecast.com>
2017-05-24 09:29:06 +0100

    I - status filter for list

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,7 +1,8 @@
 <template>
     <div>
+        <span @click="changeStatus('previous')"><<</span> {{ status }} <span @click="changeStatus('next')">>></span>
         <ul class="list-group">
-            <li v-for="movie in movies" class="list-group-item">
+            <li v-for="movie in movies" v-if="status === movie.status || status === 'All'" class="list-group-item">
                 <span>{{ movie.title }}</span>
                 <span class="pull-right delete-btn" @click="deleteMovie(movie._id)">X</span>
             </li>
@@ -11,14 +12,38 @@
 
 <script>
     const constants = require('../helpers').default.constants;
+    const renderingOptions = [
+        'Pending',
+        'Watched',
+        'All'
+    ];
+    let index = 0;
 
     export default {
+        data() {
+            return {
+                status: renderingOptions[index]
+            }
+        },
         props: [ 'movies' ],
         methods: {
             deleteMovie(id) {
                 this.$http.delete(constants.endpoint + '/' + id).then(response => {
                     this.$emit('movieDeleted', response.data);
                 });
+            },
+            changeStatus(direction) {
+                if (direction === 'next') {
+                    if (index + 1 < renderingOptions.length) {
+                        index = index + 1;
+                        this.status = renderingOptions[index];
+                    }
+                } else {
+                    if (index - 1 >= 0) {
+                        index = index - 1;
+                        this.status = renderingOptions[index];
+                    }
+                }
             }
         }
     }
